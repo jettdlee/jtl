093519c6b7b6965a35d182ab80c0978cf1b7cb8c
jettdlee <jettdlee@gmail.com>
2020-04-19 20:17:54 +0100

    reorginize some files and add avatar

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    )
+  }
+}
+
+export default Header;
