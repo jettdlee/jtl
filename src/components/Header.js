5d671546962f1bf06aac918fc2dd81d046040d0c
jettdlee <jettdlee@gmail.com>
2020-04-22 21:10:19 +0100

    more changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,15 @@ class Header extends React.Component {
   render() {
     return (
       <header className="App-header">
-        <img className="avatar-logo" src={Avataaars} alt="" />
-        <div className="headings">
-          <h1 className='heading'>
-            Hello, I'm Jet
-          </h1>
+        <div className="flex-column">
+          <div className="d-flex flex-row align-items-center">
+            <img className="avatar-logo" src={Avataaars} alt="" />
+            <div className="headings">
+              <h1 className='heading'>
+                Hello, I'm Jet
+              </h1>
+            </div>
+          </div>
           <div className='sub-header'>
             <p>Back-end Developer</p>
             <p>Ruby on Rails</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,15 @@ import '../styles/Navbar.css';
 class Navbar extends React.Component {
   render() {
     return(
-      <nav className="navbar">
-        <ul className="navbar-ul">
-          <li className="navbar-list-item">About</li>
-          <li className="navbar-list-item">Experience</li>
-          <li className="navbar-list-item">Portfolio</li>
-          <li className="navbar-list-item">Contact</li>
-        </ul>
+      <nav className="navbar" id="main_navbar">
+        <div className="container">
+          <ul className="navbar-ul horizontal">
+            <li className="navbar-list-item">About</li>
+            <li className="navbar-list-item">Experience</li>
+            <li className="navbar-list-item">Portfolio</li>
+            <li className="navbar-list-item">Contact</li>
+          </ul>
+        </div>
       </nav>
     )
   }
