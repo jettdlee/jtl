95357da930e4a79cbf2bd2ceeb72518f1f2a3224
jettdlee <jettdlee@gmail.com>
2020-05-14 20:49:43 +0100

    Updates

---
+++ precache-manifest.ffc8ca251917169a888d900681db11c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587ec60ce198f3446ae75913aec479b3",
+    "url": "/jtl/index.html"
+  },
+  {
+    "revision": "176be0a2caa72f930409",
+    "url": "/jtl/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a7705dc1cd03109c545c",
+    "url": "/jtl/static/css/main.6bf38c91.chunk.css"
+  },
+  {
+    "revision": "176be0a2caa72f930409",
+    "url": "/jtl/static/js/2.ec649462.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jtl/static/js/2.ec649462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7705dc1cd03109c545c",
+    "url": "/jtl/static/js/main.208ed7d2.chunk.js"
+  },
+  {
+    "revision": "ea495f7555915b762238",
+    "url": "/jtl/static/js/runtime-main.dbcdc0b0.js"
+  }
+]);
